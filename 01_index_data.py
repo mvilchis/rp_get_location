cc5d91f540b560ee53ce206ae5efd77d073cbf69
mvilchis <mvilchis@ciencias.unam.mx>
2017-10-31 17:44:08 -0600

    Clean code

--- 01_index_data.py
+++ 01_index_data.py
@@ -5,12 +5,9 @@ import os
 INDEX_SETTINGS = { "settings":{
                         "analysis": {
                             "filter": {
-                                #"spanish_stop"   : { "type":"stop", "stopwords":  "_spanish_" },
-                                #"spanish_stemmer": { "type": "stemmer", "language":"light_spanish" },
                                 "dbl_metaphone"  : {  "type":"phonetic","encoder": "double_metaphone","languageset":"spanish"}
                             },
                             "analyzer": {
-                                #"spanish": { "tokenizer":  "standard", "filter":[ "lowercase", "spanish_stop", "spanish_stemmer" ]},
                                 "dbl_metaphone": { "tokenizer": "standard", "filter":    "dbl_metaphone"}
                             }
                         }
@@ -20,9 +17,13 @@ INDEX_SETTINGS = { "settings":{
 
 
 NUMBERS = {'primero':1, 'segundo':2, 'tercero':3, 'cuarto':4,
-           'uno':1, 'dos':2, 'tres':3, 'cuatro':4, 'cinco':5, 'seis':6, 'siete': 7, 'ocho':8, 'nueve':9, 'diez':10,
-           'once':11, 'doce':12, 'trece':13, 'catorce':14, 'quince':15, 'dieciseis':16, 'diecisiete':17, 'dieciocho':18, 'diecinueve':19, 'veinte':20,
-           'ventiuno':21, 'veintidos':22, 'veintitres':23, 'veinticuatro':24, 'veinticinco':25, 'veintiseis':26, 'veintisiete':27, 'veintiocho':28, 'veintinueve':29, 'treinta':30, 'treinta y uno':31}
+           'uno':1, 'dos':2, 'tres':3, 'cuatro':4, 'cinco':5,
+           'seis':6, 'siete': 7, 'ocho':8, 'nueve':9, 'diez':10,
+           'once':11, 'doce':12, 'trece':13, 'catorce':14, 'quince':15,
+           'dieciseis':16, 'diecisiete':17, 'dieciocho':18,
+           'diecinueve':19, 'veinte':20, 'ventiuno':21, 'veintidos':22,
+           'veintitres':23, 'veinticuatro':24, 'veinticinco':25, 'veintiseis':26,
+           'veintisiete':27, 'veintiocho':28, 'veintinueve':29, 'treinta':30, 'treinta y uno':31}
 
 MONTHS  =  {'enero': 1, 'febrero': 2, 'marzo': 3, 'abril': 4, 'mayo': 5,
             'junio': 6, 'julio': 7, 'agosto': 8, 'septiembre': 9, 'octubre': 10,
